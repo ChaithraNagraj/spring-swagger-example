d8b41836eaf41c5939c3714a96993aebd75df829
chaithra-harshi <chaithrabn96@gmail.com>
2020-05-11 16:31:58 +0530

    [chaithra]adding:spring-swagger-eaxample

---
+++ spring-swagger-example/src/test/java/com/bridgelabz/swagger/SpringSwaggerExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.swagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSwaggerExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
